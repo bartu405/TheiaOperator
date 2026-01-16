861d5f4627ef324077c90f72454e1f23d8c8dbf8
bartu405 <bartukacar@gmail.com>
2026-01-16 13:02:41 +0300

    Update

--- src/main/kotlin/com/globalmaksimum/operator/config/OperatorConfig.kt
+++ src/main/kotlin/com/globalmaksimum/operator/config/OperatorConfig.kt
@@ -9,14 +9,14 @@ data class OperatorConfig(
     val keycloakRealm: String? = null,
     val keycloakClientId: String? = null,
 
-    val instancesHost: String? = null,      // e.g. designer.172.x...sslip.io
-    val appId: String? = null,              // e.g. henkan
+    val instancesHost: String? = null,
+    val appId: String? = null,
 
     val oAuth2ProxyVersion: String? = null,
 
-    val storageClassName: String? = null,   // e.g. local-path
-    val requestedStorage: String? = null,   // e.g. 250Mi
+    val storageClassName: String? = null,
+    val requestedStorage: String? = null,
 
-    val sessionsPerUser: Int? = null,       // ignore for now if you want
-    val serviceUrl: String? = null,         // Henkan had https://<nil> - optional
+    val sessionsPerUser: Int? = null,
+    val serviceUrl: String? = null, // Henkan had https://<nil> looks like a bug
 )
